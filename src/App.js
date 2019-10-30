c6b6631ce10d5dc4aa6359e660a74cc54b23b8f0
cynthiameiring <cynthia.meiring@hotmail.com>
2019-10-30 17:21:09 +0100

    Creating detailpage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
 import "./App.css";
 import ProductListContainer from "./components/ProductListContainer";
+import ProductDetailPageContainer from "./components/ProductDetailPageContainer";
+import { Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <p>Our amazing webshop</p>
-      <ProductListContainer />
+      <Route exact path="/" component={ProductListContainer} />
+      <Route
+        exact
+        path="/products/:id"
+        component={ProductDetailPageContainer}
+      />
     </div>
   );
 }
