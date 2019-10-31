41a0326604857f84d83a568cd0e42a942d9001d0
krik-chry <krikonis.chrisostomos@gmail.com>
2019-10-31 16:56:10 +0100

    Changes to ProducList

--- src/components/ProductDetailPageContainer.js
+++ src/components/ProductDetailPageContainer.js
@@ -5,18 +5,10 @@ import ProductDetailPage from "./ProductDetailPage";
 class ProductDetailPageContainer extends React.Component {
   render() {
     const productId = this.props.match.params.id;
-    console.log("test1", productId);
-    console.log("What is products?", this.props.products);
-    // console.log("test2", this.props.products[4].id);
 
     const product = this.props.products.find(
       element => element.id === Number(productId)
     );
-
-    //const product = this.props.products[productId - 1];
-
-    console.log("test3", product);
-
     return (
       <ProductDetailPage
         productName={product.name}
