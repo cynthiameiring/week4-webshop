97a191f2c2d6d9e8d7d784518d6071cc9c64aa86
krik-chry <krikonis.chrisostomos@gmail.com>
2019-10-31 09:46:43 +0100

    Clothes data

---
+++ src/reducers/clothes.js
@@ -0,0 +1,92 @@
+const clothes = [
+  {
+    id: 1,
+    categoryId: 2,
+    name: "Slim Jeans",
+    description: "Jeans in washed denim with a regular waist, zip fly, and slim legs.",
+    imageUrl: "https://lp2.hm.com/hmgoepprod?set=source[/36/49/3649b9506787743b7d4b0e5f8014d13946bf3632.jpg],origin[dam],category[men_jeans_slim],type[DESCRIPTIVESTILLLIFE],res[x],hmver[2]&call=url[file:/product/fullscreen]",
+    inStock: true,
+    price: "35"
+  },
+  {
+    id: 2,
+    categoryId: 2,
+    name: "Skinny Jeans",
+    description: "Jeans in washed stretch denim with heavily distressed details. Regular waist, zip fly with button, and skinny legs. Made partly from recycled cotton.",
+    imageUrl: "https://lp2.hm.com/hmgoepprod?set=source[/3f/1e/3f1ead4373aa773d16b854fe9366edb5ce62d023.jpg],origin[dam],category[men_jeans_skinny],type[DESCRIPTIVESTILLLIFE],res[x],hmver[1]&call=url[file:/product/fullscreen]",
+    inStock: true,
+    price: "49"
+  },
+  {
+    id: 3,
+    categoryId: 2,
+    name: "Cotton Flannel Shirt",
+    description: "Plaid shirt in soft cotton flannel. Turn-down collar, classic button placket, yoke at back, and two open chest pockets. Long sleeves with adjustable buttoning at cuffs. Rounded hem.",
+    imageUrl: "https://lp2.hm.com/hmgoepprod?set=source[/67/9c/679cc6d85d497f268eca5931ec7e4f591c726e8b.jpg],origin[dam],category[],type[DESCRIPTIVESTILLLIFE],res[x],hmver[1]&call=url[file:/product/fullscreen]",
+    inStock: true,
+    price: "28"
+  },
+  {
+    id: 4,
+    categoryId: 2,
+    name: "Hooded Sweatshirt",
+    description: "Long-sleeved sweatshirt in soft fabric with a lined drawstring hood, kangaroo pocket, and ribbing at cuffs and hem. Soft, brushed inside.",
+    imageUrl: "https://lp2.hm.com/hmgoepprod?set=source[/ca/77/ca771008369f7523aa8d7b6685781eac533ddb60.jpg],origin[dam],category[men_hoodiessweatshirts_hoodies],type[DESCRIPTIVESTILLLIFE],res[s],hmver[1]&call=url[file:/product/main]",
+    inStock: true,
+    price: "21"
+  },
+  {
+    id: 5,
+    categoryId: 2,
+    name: "Cardigan with Zip",
+    description: "Soft cardigan in ribbed jersey with a small stand-up collar, zip at front, and side pockets. Ribbing at cuffs and hem.",
+    imageUrl: "https://lp2.hm.com/hmgoepprod?set=source[/69/21/69214765fc84146e88557b629849d7fa50d8ad72.jpg],origin[dam],category[men_cardigansjumpers_cardigans],type[DESCRIPTIVESTILLLIFE],res[s],hmver[1]&call=url[file:/product/main]",
+    inStock: true,
+    price: "32"
+  },
+  {
+    id: 6,
+    categoryId: 2,
+    name: "Oversized Turtleneck Sweater",
+    description: "Soft, oversized, knit sweater with wool content. Ribbed turtleneck, heavily dropped shoulders, and long sleeves. Ribbing at cuffs and hem. Polyester content is recycled.",
+    imageUrl: "https://lp2.hm.com/hmgoepprod?set=source[/1f/1e/1f1e1927edee1a6935d62fe37cd2eac11bd30733.jpg],origin[dam],category[],type[DESCRIPTIVESTILLLIFE],res[y],hmver[1]&call=url[file:/product/main]",
+    inStock: true,
+    price: "34"
+  },
+  {
+    id: 7,
+    categoryId: 2,
+    name: "Long Jacket",
+    description: "Long jacket in jersey with narrow, notched lapels and front pockets. Unlined.",
+    imageUrl: "https://lp2.hm.com/hmgoepprod?set=source[/85/af/85af9d46ab60e164e83c52f13c70b9ae30968236.jpg],origin[dam],category[ladies_blazerswaistcoats_blazers],type[DESCRIPTIVESTILLLIFE],res[x],hmver[1]&call=url[file:/product/fullscreen]",
+    inStock: true,
+    price: "25"
+  },
+  {
+    id: 8,
+    categoryId: 2,
+    name: "Jersey Skirt with Slit",
+    description: "Fitted, calf-length skirt in jersey with a high waist, elasticized waistband, and slit at front.",
+    imageUrl: "https://lp2.hm.com/hmgoepprod?set=source[/81/e5/81e5c0b80aaa6c4b2afe96defa5c92103fec7f44.jpg],origin[dam],category[ladies_skirts_midiskirts],type[DESCRIPTIVESTILLLIFE],res[x],hmver[1]&call=url[file:/product/fullscreen]",
+    inStock: true,
+    price: "16"
+  },
+  {
+    id: 9,
+    categoryId: 2,
+    name: "Long Pile Coat",
+    description: "Long coat in soft pile with notched lapels. Concealed snap fasteners at front, concealed side-seam pockets, and long raglan sleeves. Lined.",
+    imageUrl: "https://lp2.hm.com/hmgoepprod?set=source[/d0/27/d027a31d51ee5e9f8051b47d74975723febfeb33.jpg],origin[dam],category[ladies_jacketscoats_coats],type[DESCRIPTIVESTILLLIFE],res[s],hmver[1]&call=url[file:/product/main]",
+    inStock: true,
+    price: "95"
+  },
+  {
+    id: 10,
+    categoryId: 2,
+    name: "Short Mock-turtleneck Top",
+    description: "Short, fitted top in ribbed jersey with a mock turtleneck and long sleeves.",
+    imageUrl: "https://lp2.hm.com/hmgoepprod?set=source[/73/4f/734f84560a701324180233d67c7c79ec95882f1f.jpg],origin[dam],category[],type[DESCRIPTIVESTILLLIFE],res[s],hmver[1]&call=url[file:/product/main]",
+    inStock: true,
+    price: "14"
+  }
+];
